17c27e2ecee77c10cf54594ddbb9c981609697d9
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-02 16:25:40 +0100

    add labo 2 exercise 3

---
+++ src/labo_2/ex_3.cpp
@@ -0,0 +1,21 @@
+import std;
+
+int main() // NOLINT(bugprone-exception-escape)
+{
+    static constexpr int min_number{0};
+    static constexpr int max_number{4};
+
+    std::println("Geef een getal van 0 tem 4: ");
+    int number{};
+    std::cin >> number;
+
+    if ((min_number <= number) && (number <= max_number))
+    {
+        std::vector<std::string> const numbers{"nul", "een", "twee", "drie", "vier"};
+        std::println("{} in tekst is {}", number, numbers[static_cast<std::size_t>(number)]);
+    }
+    else
+    {
+        std::println("Kan het getal niet converteren!");
+    }
+}
