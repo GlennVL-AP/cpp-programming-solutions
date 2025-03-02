2edcb1a838215947d20950bc09592de07b44c9be
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-02 16:04:29 +0100

    add labo 2 exercise 1

---
+++ src/labo_2/ex_1.cpp
@@ -0,0 +1,14 @@
+import std;
+
+int main() // NOLINT(bugprone-exception-escape)
+{
+    static constexpr int months_per_year{12};
+
+    std::println("Geef je voornaam en leeftijd: ");
+
+    std::string first_name{};
+    int age{};
+    std::cin >> first_name >> age;
+
+    std::println("Dag {}, je bent {} maanden oud.", first_name, age * months_per_year);
+}
