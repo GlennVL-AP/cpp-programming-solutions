b76a32b5fddb541c5b44b925fedd10d534b42312
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-02 17:24:22 +0100

    add labo 3 exercise 2

---
+++ src/labo_3/ex_2.cpp
@@ -0,0 +1,24 @@
+import cpprog;
+import std;
+
+double ctok(double celsius)
+{
+    static constexpr double kelvin_offset{273.15};
+    cpprog::expect([&] { return -kelvin_offset <= celsius; }, "Temperature can't be below absolute zero!");
+    double const kelvin{celsius + kelvin_offset};
+    return kelvin;
+}
+
+int main()
+try
+{
+    double celsius{0.0};
+    std::cin >> celsius;
+    double kelvin = ctok(celsius);
+    std::println("{}", kelvin);
+}
+catch (std::exception const& e)
+{
+    std::cerr << e.what() << '\n';
+    return 1;
+}
