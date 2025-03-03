5ad9f9ef8d64e4b50ecd8e13d083d8643bce4e6e
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-03 15:52:45 +0100

    add labo 4 exercise 7

---
+++ src/labo_4/ex_7.cpp
@@ -0,0 +1,16 @@
+import datetime;
+import std;
+
+static_assert(std::is_class_v<datetime::Date>);
+static_assert(std::is_standard_layout_v<datetime::Date>);
+static_assert(std::is_default_constructible_v<datetime::Date>);
+static_assert(std::is_trivially_destructible_v<datetime::Date>);
+static_assert(std::is_copy_constructible_v<datetime::Date>);
+static_assert(std::is_copy_assignable_v<datetime::Date>);
+static_assert(std::is_trivially_copyable_v<datetime::Date>);
+static_assert(std::is_move_constructible_v<datetime::Date>);
+static_assert(std::is_move_assignable_v<datetime::Date>);
+
+int main()
+{
+}
