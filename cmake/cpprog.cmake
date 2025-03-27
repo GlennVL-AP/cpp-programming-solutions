00311f6dd37c25bad31318805b6395ecff9585cf
Glenn Van Loon (p151886) <glenn.vanloon@ap.be>
2025-03-27 22:26:53 +0100

    use compact test result reporter

--- cmake/cpprog.cmake
+++ cmake/cpprog.cmake
@@ -106,11 +106,7 @@ function(cpprog_add_test)
     target_link_libraries(${arg_TARGET} PRIVATE Catch2::Catch2WithMain cpprog ${arg_DEPENDENCIES})
     _cpprog_set_compiler_options(TARGET ${arg_TARGET})
     _cppprog_enable_sanitizers(TARGET ${arg_TARGET})
-    catch_discover_tests(${arg_TARGET}
-        EXTRA_ARGS
-        --reporter JUnit::out=${arg_TARGET}.junit.xml
-        --reporter compact::out=-::colour-mode=ansi
-    )
+    catch_discover_tests(${arg_TARGET} REPORTER compact)
 endfunction()
 
 function(_cpprog_set_compiler_options)
